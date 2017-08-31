d0dfe7e74f8c2088114f2dbdff94bcd23692ac82
David Mandelberg <david@mandelberg.org>
2017-08-31 01:20:36 -0400

    Simplify collapsing javascript.
    Always explicitly set or clear the "checked" property, regardless of its
    initial value. This makes it possible for the following commit to default posts
    to uncollapsed.
    
    Additionally, use a fixed length above which to collapse, instead of a factor.
    This is mostly because we can't rely on clientHeight being limited to the
    max-height anymore.

--- collapsing.js
+++ collapsing.js
@@ -1,14 +1,10 @@
 jQuery(function() {
-  var expand_up_to = 1.5;
+  // A post will be collapsed iff it's taller than collapse_if_above.
+  var rem_to_px = jQuery("<div style=\"width: 1rem;\"></div>").width();
+  var collapse_if_above = 450 * rem_to_px;
 
-  jQuery(".dseomn-is-collapsed:checked").each(function() {
+  jQuery(".dseomn-is-collapsed").prop("checked", function() {
     var content = jQuery(this).next(".entry").find(".entry-content").get(0);
-    if (content === undefined) {
-      return;
-    }
-
-    if (content.scrollHeight < expand_up_to * content.clientHeight) {
-      this.checked = false;
-    }
+    return content !== undefined && content.scrollHeight > collapse_if_above;
   });
 });
